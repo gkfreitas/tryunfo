df4dffb7dc91eb924b5c474d7b55a71687dd092d
Gabriel <gabrielkelvinfreitas@gmail.com>
2023-01-27 17:19:44 -0300

    REQ5 - Finalizado, botão salvar validado!

--- src/App.js
+++ src/App.js
@@ -6,17 +6,18 @@ class App extends React.Component {
   constructor() {
     super();
     this.onInputChange = this.onInputChange.bind(this);
+    this.buttonDisabled = this.buttonDisabled.bind(this);
     this.state = {
       cardName: '',
       cardDescription: '',
-      cardAttr1: '',
-      cardAttr2: '',
-      cardAttr3: '',
+      cardAttr1: '0',
+      cardAttr2: '0',
+      cardAttr3: '0',
       cardImage: '',
       cardRare: '',
       cardTrunfo: false,
       hasTrunfo: false,
-      isSaveButtonDisabled: false,
+      isSaveButtonDisabled: true,
     };
   }
 
@@ -24,6 +25,37 @@ class App extends React.Component {
     const { name, value } = target;
     this.setState({
       [name]: value,
+    }, this.buttonDisabled);
+  }
+
+  buttonDisabled() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+    } = this.state;
+    const maxNumber = 90;
+    const minNumber = 0;
+    const maxSumNumber = 210;
+    const disabledCases = [
+      !cardName.length,
+      !cardImage.length,
+      !cardDescription.length,
+      cardAttr1 > maxNumber,
+      cardAttr1 < minNumber,
+      cardAttr2 > maxNumber,
+      cardAttr2 < minNumber,
+      cardAttr3 > maxNumber,
+      cardAttr3 < minNumber,
+      parseInt(cardAttr1, 10)
+      + parseInt(cardAttr2, 10) + parseInt(cardAttr3, 10) > maxSumNumber,
+    ];
+    const verify = disabledCases.every((error) => error === false);
+    this.setState({
+      isSaveButtonDisabled: !verify,
     });
   }
 
@@ -37,6 +69,7 @@ class App extends React.Component {
       cardImage,
       cardRare,
       cardTrunfo,
+      isSaveButtonDisabled,
     } = this.state;
     return (
       <div>
@@ -50,6 +83,7 @@ class App extends React.Component {
           cardImage={ cardImage }
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
+          isSaveButtonDisabled={ isSaveButtonDisabled }
         />
         <Card
           cardName={ cardName }
